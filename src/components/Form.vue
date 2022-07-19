73b7eebfcbc0b5b139fdf70df0f8f43459948ce9
Vladimir2ht <vovekus@mail.ru>
2022-07-19 16:59:32 +0500

    Vuetify and layout

---
+++ src/components/Form.vue
@@ -0,0 +1,162 @@
+<template>
+		<v-dialog 
+			v-model="dialog"
+			persistent
+			max-width="600px"
+		>
+			<template v-slot:activator="{ on, attrs }">
+				<v-btn
+					dark
+					color="blue"
+					rounded
+					v-bind="attrs"
+					v-on="on"
+				>
+					Добавить сотрудника
+				</v-btn>
+
+			</template>
+			<v-card>
+				<v-card-title>
+					<span class="text-h5">Профиль сотрудника</span>
+				</v-card-title>
+				<v-card-text>
+					<v-container>
+						<v-row>
+							<v-col
+								cols="12"
+								sm="6"
+								md="4"
+							>
+								<v-text-field
+									label="Имя"
+									required
+								></v-text-field>
+							</v-col>
+							<v-col
+								cols="12"
+								sm="6"
+								md="4"
+							>
+								<v-text-field
+									label="Отчество"
+									required
+								></v-text-field>
+							</v-col>
+							<v-col
+								cols="12"
+								sm="6"
+								md="4"
+							>
+								<v-text-field
+									label="Фамилия"
+									required
+								></v-text-field>
+							</v-col>
+							<v-col
+								cols="12"
+								sm="6"
+								md="4"
+							>
+								<v-text-field
+									label="Должность"
+									required
+								></v-text-field>
+							</v-col>
+							<v-col
+								cols="12"
+								sm="6"
+								md="4"
+							>
+								<v-text-field
+									label="Оклад"
+									required
+								></v-text-field>
+							</v-col>
+							<v-col
+								cols="12"
+								sm="6"
+								md="4"
+							>
+
+								<v-menu
+									v-model="menu2"
+									:close-on-content-click="false"
+									:nudge-right="40"
+									transition="scale-transition"
+									offset-y
+									min-width="auto"
+								>
+									<template v-slot:activator="{ on, attrs }">
+										<v-text-field
+											v-model="date"
+											label="Поступил(а) на работу"
+											prepend-icon="mdi-calendar"
+											readonly
+											v-bind="attrs"
+											v-on="on"
+										></v-text-field>
+									</template>
+									<v-date-picker
+										v-model="date"
+										@input="menu2 = false"
+										show-adjacent-months
+										required
+									></v-date-picker>
+								</v-menu>
+							</v-col>
+
+							<v-col
+								cols="12"
+								sm="6"
+							>
+								<v-select
+									:items="['0.5', 'полная']"
+									label="Ставка"
+									required
+								></v-select>
+							</v-col>
+							<v-col
+								cols="12"
+								sm="6"
+							>
+								<v-checkbox
+									label="Трудовая книжка сдана"
+								></v-checkbox>
+							</v-col>
+						</v-row>
+					</v-container>
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn
+						color="blue darken-1"
+						text
+						@click="dialog = false"
+					>
+						Закрыть
+					</v-btn>
+					<v-btn
+						color="blue darken-1"
+						text
+						@click="dialog = false"
+					>
+						Сохранить
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+
+</template>
+
+<script>
+	export default {
+		name: 'MainForm',
+
+		data: () => ({
+			dialog: false,
+			date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      menu2: false,
+		}),
+	}
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,110 @@
+<template>
+  
+<v-data-table
+    :headers="headers"
+    :items="desserts"
+    class="elevation-1"
+    hide-default-footer
+    hide-default-header
+    disable-sort
+  >
+      <template
+        v-slot:header="{ props: { headers } }"
+      >
+        <thead>
+          <tr>
+            <th>
+              {{headers[0].text}}
+            </th>
+            <th>
+              {{headers[1].text}}
+            </th>
+            <th>
+              {{headers[2].text}}
+            </th>
+            <th>
+              {{headers[3].text}}
+            </th>
+            <th>
+              {{headers[4].text}}
+            </th>
+            <th>
+              <Form>
+                Добавить сотрудника
+              </Form>
+            </th>
+          </tr>
+        </thead>
+      </template>
+
+    <template v-slot:item="row">
+      <tr>
+        <td>{{row.item.number}}</td>
+        <td>{{row.item.name}}</td>
+        <td>{{row.item.family}}</td>
+        <td>{{row.item.father}}</td>
+        <td>{{row.item.work}}</td>
+        <td>
+          <v-btn class="mx-2" rounded dark small color="blue" @click="ChangeWorker(row.item)">
+            Обновить
+          </v-btn>
+          <v-btn class="mx-2" rounded dark small color="red" @click="DeleteWorker(row.item)">
+            Удалить
+          </v-btn>
+        </td>
+      </tr>
+    </template>
+  
+  </v-data-table>
+
+</template>
+
+<script>
+import Form from './Form';
+
+  export default {
+    name: 'MainTable',
+    
+    components: {
+      Form,
+    },
+    
+    data: () => ({
+      headers: [
+          {
+            text: '№', value: 'number',
+            align: 'start',
+            sortable: false,
+          },
+          { text: 'Имя', value: 'name', },
+          { text: 'Фамилия', value: 'favily' },
+          { text: 'Отчество', value: 'father' },
+          { text: 'Должность', value: 'work' },
+          { value: 'button' },
+        ],
+        desserts: [
+          {
+            number: 0,
+            name: 'Frozen',
+            family: 'Mohov',
+            father: 'Rexov',
+            work: 'Builder',
+          },
+          {
+            number: 1,
+            name: 'Froze',
+            family: 'Moho',
+            father: 'Rexo',
+            work: 'Builde',
+          },
+          {
+            number: 2,
+            name: 'Froz',
+            family: 'Moh',
+            father: 'Rex',
+            work: 'Build',
+          },
+        ],
+    }),
+  }
+</script>
